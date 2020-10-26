8eaeb57889cc6c78b0c14b0ede6d3550eff8ebd1
Nikita Volkov <nikitavolkov1707@gmail.com>
2020-10-26 14:37:33 +0300

    испрвлена реализация последовательного алгоритма, согласно условиям, убраны лишние функции

--- matrix.c
+++ matrix.c
@@ -9,18 +9,27 @@ Matrix* create_matrix_from_file(const char* path_file) {
   fp = fopen(path_file, "r+");
 
   if (fp == NULL) {
-    printf("%s\n", "Can't open file");
     return NULL;
   }
 
   Matrix* my_matrix = (Matrix*)malloc(sizeof(Matrix));
+  if (my_matrix == NULL) {
+    return NULL;
+  }
 
   fscanf(fp, "%zu", &my_matrix->num_rows);
   fscanf(fp, "%zu", &my_matrix->num_cols);
 
   my_matrix->matrix = (double**)malloc(my_matrix->num_rows * sizeof(double*));
+  if (my_matrix->matrix == NULL) {
+    return NULL;
+  }
   for (size_t i = 0; i < my_matrix->num_rows; ++i) {
     my_matrix->matrix[i] = (double*)malloc(my_matrix->num_cols * sizeof(double));
+
+    if (my_matrix->matrix[i] == NULL) {
+      return NULL;
+    }
   }
 
   for (size_t i = 0; i < my_matrix->num_rows; ++i) {
@@ -35,18 +44,27 @@ Matrix* create_matrix_from_file(const char* path_file) {
 
 Matrix* create_matrix(size_t rows, size_t cols) {
   if (rows == 0 || cols == 0) {
-    puts("Wrong number of rows or cols");
     return NULL;
   }
 
   Matrix* my_matrix = (Matrix*)malloc(sizeof(Matrix));
+  if (my_matrix == NULL) {
+    return NULL;
+  }
 
   my_matrix->num_rows = rows;
   my_matrix->num_cols = cols;
 
   my_matrix->matrix = (double**)malloc(rows * sizeof(double*));
+  if (my_matrix->matrix == NULL) {
+    return NULL;
+  }
   for (size_t i = 0; i < rows; ++i) {
     my_matrix->matrix[i] = (double*)malloc(cols * sizeof(double));
+
+    if (my_matrix->matrix[i] == NULL) {
+      return NULL;
+    }
   }
 
   return my_matrix;
@@ -54,7 +72,6 @@ Matrix* create_matrix(size_t rows, size_t cols) {
 
 int get_rows(const Matrix* matrix, size_t* rows) {
   if (matrix == NULL || rows == NULL) {
-    puts("Error");
     return EXIT_FAILURE;
   }
 
@@ -64,7 +81,6 @@ int get_rows(const Matrix* matrix, size_t* rows) {
 
 int get_cols(const Matrix* matrix, size_t* cols) {
   if (matrix == NULL || cols == NULL) {
-    puts("Error");
     return EXIT_FAILURE;
   }
 
@@ -72,38 +88,8 @@ int get_cols(const Matrix* matrix, size_t* cols) {
   return EXIT_SUCCESS;
 }
 
-int get_elem(const Matrix* matrix, size_t row, size_t col, double* val) {
-  if (matrix == NULL || val == NULL) {
-    puts("Error");
-    return EXIT_FAILURE;
-  }
-  if (row >= matrix->num_rows || col >= matrix->num_cols) {
-    puts("Wrong number of row or col");
-    return EXIT_FAILURE;
-  }
-
-  *val = matrix->matrix[row][col];
-  return EXIT_SUCCESS;
-}
-
-int set_elem(Matrix* matrix, size_t row, size_t col, double val) {
-  if (matrix == NULL) {
-    puts("Error");
-    return EXIT_FAILURE;
-  }
-
-  if (row >= matrix->num_rows || col >= matrix->num_cols) {
-    puts("Wrong number of row or col");
-    return EXIT_FAILURE;
-  }
-
-  matrix->matrix[row][col] = val;
-  return EXIT_SUCCESS;
-}
-
 int free_matrix(Matrix* matrix) {
   if (matrix == NULL) {
-    puts("Error");
     return EXIT_FAILURE;
   }
 
--- matrix.h
+++ matrix.h
@@ -3,7 +3,6 @@
 
 #include <stddef.h>
 
-
 typedef struct Matrix {
   size_t num_rows;
   size_t num_cols;
@@ -18,8 +17,6 @@ int free_matrix(Matrix* matrix);
 // Basic operations
 int get_rows(const Matrix* matrix, size_t* rows);
 int get_cols(const Matrix* matrix, size_t* cols);
-int get_elem(const Matrix* matrix, size_t row, size_t col, double* val);
-int set_elem(Matrix* matrix, size_t row, size_t col, double val);
 
 
 #endif  // TP_C_CPP_HOMEWORK__MATRIX_H_
