cce53ae6eafec331013cdb245b48136d08461214
Nikita Volkov <nikitavolkov1707@gmail.com>
2020-10-26 14:39:24 +0300

    испрвлена реализация последовательного алгоритма, согласно условиям

--- sequential_column_sum.c
+++ sequential_column_sum.c
@@ -1,9 +1,7 @@
 #include "sequential_column_sum.h"
 #include "matrix.h"
 
-#include <stdio.h>
 #include <stdlib.h>
-
 
 double* find_column_sum_sequentially(const Matrix* matrix) {
   size_t number_of_columns = 0;
@@ -19,8 +17,8 @@ double* find_column_sum_sequentially(const Matrix* matrix) {
   if (get_rows(matrix, &number_of_rows)) {
     return NULL;
   }
-  for (size_t i = 0; i < number_of_rows; ++i) {
-    for (size_t j = 0; j < number_of_columns; ++j) {
+  for (size_t j = 0; j < number_of_columns; ++j) {
+    for (size_t i = 0; i < number_of_rows; ++i) {
       sum_every_column[j] += matrix->matrix[i][j];
     }
   }
